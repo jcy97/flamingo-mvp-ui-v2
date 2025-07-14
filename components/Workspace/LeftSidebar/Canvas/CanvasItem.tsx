be42d9de3a6bbd032492db6d8409b92ee7a872d6
jcy97 <sook3427@naver.com>
2025-07-14 14:34:13 +0900

    [feat] 캔버스 아이템 테두리 추가

--- components/Workspace/LeftSidebar/Canvas/CanvasItem.tsx
+++ components/Workspace/LeftSidebar/Canvas/CanvasItem.tsx
@@ -9,7 +9,7 @@ interface CanvasItemProps {
 
 function CanvasItem({ data, isSelected }: CanvasItemProps): React.ReactElement {
   return (
-    <div className="w-[85%] aspect-[4/3] min-w-[140px] rounded-xl border border-neutral-500 flex flex-col">
+    <div className="w-[85%] aspect-[4/3] min-w-[140px] rounded-xl border border-neutral-500 flex flex-col cursor-pointer hover:border-primary-500 hover:border-2">
       <div className="h-[20%] min-h-[25px] bg-neutral-700 rounded-tl-xl rounded-tr-xl px-2 flex items-center justify-between">
         <span className="text-ellipsis overflow-hidden whitespace-nowrap flex-1 mr-2 text-sm">
           {data.name}
